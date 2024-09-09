8a9fcb609ea2f8a0eabda23720f7977cbc9b34c3
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-09-10 00:43:58 +0200

    converting strings to numbers list.

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-def check_if_symmetric(word):
-    word = word.lower()
-    reversed_word = word[::-1]
-    print(word)
-    print(reversed_word)
-    if word == reversed_word:
-        return True
-    else:
-        return False
+# def check_if_symmetric(word):
+#     word = word.lower()
+#     reversed_word = word[::-1]
+#     print(word)
+#     print(reversed_word)
+#     if word == reversed_word:
+#         return True
+#     else:
+#         return False
 
-word = input("Enter a word: ")
-print(check_if_symmetric(word))
+# word = input("Enter a word: ")
+# print(check_if_symmetric(word))
+
+
+def convert_to_numbers(string):
+    alphabets = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    output_array = []
+    for char in string:
+        output_array.append(alphabets.index(char))
+    return output_array
+
+print(convert_to_numbers("a cat"))
+
