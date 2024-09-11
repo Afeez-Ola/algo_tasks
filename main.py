77fe3ae68de04a7f3aa2370afec569e9e75f85b1
Afeez-Ola <afeezbolajiola@gmail.com>
2024-09-11 20:04:48 +0200

    wrote test for checky_if_symmetric function

--- main.py
+++ main.py
@@ -1,84 +1,104 @@
+import unittest
 def check_if_symmetric(word):
     word = word.lower()
     reversed_word = word[::-1]
-    print(word)
-    print(reversed_word)
+    # print(word)
+    # print(reversed_word)
     if word == reversed_word:
         return True
     else:
         return False
 
-word = input("Enter a word: ")
-print(check_if_symmetric(word))
+# word = input("Enter a word: ")
+# print(check_if_symmetric(word))
 
-
-def convert_to_numbers(string):
-    alphabets = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    output_array = []
-    for char in string:
-        output_array.append(alphabets.index(char))
-    return output_array
-
-print(convert_to_numbers("a cat"))
-
-
-def convert_to_numbers(num_list):
-    alphabets = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    output_char_list = []
-    for num in num_list:
-        output_char_list.append(alphabets[num])
-    output_string = "".join(output_char_list)
-    return output_string
-
-print(convert_to_numbers([1,0,3,1,20]))
-
-
-def get_intersection(array1, array2):
-    intersect_array = []
-    for item in array1:
-        if item in array2 and item not in intersect_array:
-            intersect_array.append(item)
-    return intersect_array
-
-print(get_intersection(array1 = [1, 3,3, 5, 7, 9],array2 = [3, 6, 7, 10]))
-
-
-
-def get_union(array1, array2):
-    union_array = []
-    for i in array1:
-        if i not in union_array:
-            union_array.append(i)
-        for j in array2:
-            if j not in union_array:
-                union_array.append(j)
-            
-    return sorted(union_array)
-
-print(get_union(array1 = [1, 3,3, 5, 7, 9],array2 = [3, 6, 7, 10]))
-
-def count_characters(string):
-  string = string.lower()
-  char_count = 0
-  char_dict= {}
-  for char in string:
-    if char in char_dict:
-      char_dict[char] += 1
-    else:
-      char_dict[char] = 1
-  return char_dict
+class TestSymmetryChecker(unittest.TestCase):
+  def not_palindrome(self):
+    self.assertFalse(check_if_symmetric("hello"))
   
-print(count_characters("A Cat"))
+  def not_palindrome(self):
+    self.assertTrue(check_if_symmetric("madam"))
+    
+  def test_mixed_case_palindrome(self):
+    self.assertTrue(check_if_symmetric("RaceCar"))
+  
+  def test_empty_string(self):
+    self.assertTrue(check_if_symmetric(""))
+    
+  def test_single_character_string(self):
+    self.assertTrue(check_if_symmetric("a"))
+  
+if __name__ == "__main__":
+  unittest.main()
+
+
+# def convert_to_numbers(string):
+#     alphabets = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+#     output_array = []
+#     for char in string:
+#         output_array.append(alphabets.index(char))
+#     return output_array
+
+# print(convert_to_numbers("a cat"))
+
+
+# def convert_to_numbers(num_list):
+#     alphabets = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+#     output_char_list = []
+#     for num in num_list:
+#         output_char_list.append(alphabets[num])
+#     output_string = "".join(output_char_list)
+#     return output_string
+
+# print(convert_to_numbers([1,0,3,1,20]))
+
+
+# def get_intersection(array1, array2):
+#     intersect_array = []
+#     for item in array1:
+#         if item in array2 and item not in intersect_array:
+#             intersect_array.append(item)
+#     return intersect_array
+
+# print(get_intersection(array1 = [1, 3,3, 5, 7, 9],array2 = [3, 6, 7, 10]))
 
 
 
-def is_prime(N):
-  isPrime = False
-  if N % 2 == 0 or N % 3 == 0:
-    isPrime = False
-  elif (N == 2 or N == 3) or N >= 1:
-    isPrime = True 
-  return isPrime
+# def get_union(array1, array2):
+#     union_array = []
+#     for i in array1:
+#         if i not in union_array:
+#             union_array.append(i)
+#         for j in array2:
+#             if j not in union_array:
+#                 union_array.append(j)
+            
+#     return sorted(union_array)
 
-print(is_prime(17))
+# print(get_union(array1 = [1, 3,3, 5, 7, 9],array2 = [3, 6, 7, 10]))
+
+# def count_characters(string):
+#   string = string.lower()
+#   char_count = 0
+#   char_dict= {}
+#   for char in string:
+#     if char in char_dict:
+#       char_dict[char] += 1
+#     else:
+#       char_dict[char] = 1
+#   return char_dict
+  
+# print(count_characters("A Cat"))
+
+
+
+# def is_prime(N):
+#   isPrime = False
+#   if N % 2 == 0 or N % 3 == 0:
+#     isPrime = False
+#   elif (N == 2 or N == 3) or N >= 1:
+#     isPrime = True 
+#   return isPrime
+
+# print(is_prime(17))
   
---
+++ test.py
@@ -0,0 +1,36 @@
+from main import check_if_symmetric
+
+tests = [
+  {
+    'function': check_if_symmetric,
+    'input': "racecar",
+    'output': True
+  },
+  {
+    'function': check_if_symmetric,
+    'input': "Hello",
+    'output': False
+  }
+]
+
+num_successes = 0
+num_failures = 0
+
+for test in tests:
+  function = test['function']
+  test_input = test['input']
+  desired_output = test['output']
+  actual_output = function(test_input)
+  
+  if actual_output == desired_output:
+    num_successes +=1
+  else:
+    num_failures +=1
+    function_name = function.__name__
+    print('')
+    print(f'{function_name} failed on input {test_input}')
+    print(f"\tActual Ouput: {actual_output}")
+    print(f'\tDesired Output: {desired_output}')
+    
+print(f'Testing complete: {num_successes} successes and {num_failures} failures.')
+    
