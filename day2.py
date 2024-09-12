1be39f80280ca7610dc5609715142ae380e40feb
Afeez-Ola <afeezbolajiola@gmail.com>
2024-09-13 00:51:04 +0200

    .

--- day2.py
+++ day2.py
@@ -1,11 +1,16 @@
 
 def binary_to_decimal(string):
   decimal_value = 0
-  for char in range(len(string)-1,-1,-1):
-    char = int(char)
-    decimal_value+=2**char
+  binary_list = [int(num) for num in string]
+  for i,num in enumerate(binary_list[::-1]):
+    index = (len(binary_list) - i - 1)
+    power_value = (2**index)
+    decimal_value = power_value * num
+    print(power_value)
   return decimal_value
 
 if __name__ == "__main__":
-  string = input("Kindly enter a binary number: ")
-  print(binary_to_decimal(string))
+  # string = input("Kindly enter a binary number: ")
+  print(binary_to_decimal("11010"))
+  
+  
