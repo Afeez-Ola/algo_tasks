627dd1aacd8b90288a87ff42b49d385488ba0393
Afeez-Ola <afeezbolajiola@gmail.com>
2024-09-13 00:57:09 +0200

    completed the first task

--- day2.py
+++ day2.py
@@ -1,16 +1,16 @@
 
-def binary_to_decimal(string):
-  decimal_value = 0
-  binary_list = [int(num) for num in string]
-  for i,num in enumerate(binary_list[::-1]):
-    index = (len(binary_list) - i - 1)
-    power_value = (2**index)
-    decimal_value = power_value * num
-    print(power_value)
-  return decimal_value
+# def binary_to_decimal(string):
+#   decimal_value = 0
+#   binary_list = [int(num) for num in string]
+#   for i,num in enumerate(binary_list[::-1]):
+#     power_value = (2**i)
+#     decimal_value += power_value * num
+#   return decimal_value
 
-if __name__ == "__main__":
-  # string = input("Kindly enter a binary number: ")
-  print(binary_to_decimal("11010"))
+# if __name__ == "__main__":
+#   string = input("Kindly enter a binary number: ")
+#   print(binary_to_decimal(string))
+
+
   
   
